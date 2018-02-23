9f1bfd94f4d4a63368777a11c975e930331a6ab4
Tanner McClain <tann3rjm129@gmail.com>
2018-02-22 21:23:02 -0800

    fronty front front

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+
+var term;
+var number;
+var startYear;
+var endYear;
+
+
+$(document).on("click", "#searchButton", function buttonSearch() {
+	
+
+				
+	// var term = $("#searchTerm").val();
+	// var number = $("#numberOfRecords").val();
+	// var startYear = $("#startYear").val();
+	// var endYear = $("#endYear").val();
+
+	console.log(term);
+
+});
