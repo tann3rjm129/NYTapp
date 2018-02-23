15677ade1082c1b1b6160c3a83371489737c5390
Apollo <jackson.harris3@gmail.com>
2018-02-22 21:25:34 -0800

    add jquery elements to get grab form data

--- assets/javascript/backend.js
+++ assets/javascript/backend.js
@@ -2,9 +2,31 @@ $(document).ready(function (){
     console.log("test")
 
     $("#searchButton").on("click", function (event) {
+        
+        //prevents submits by button
         event.preventDefault()
-        console.log("button press")
-        var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=bd2ce8623fb048b697f2ddbc2abef055&q=" + "banjo";
+        
+        //defines query from search value on form
+        var searchTerm = $("#searchTerm").val();
+        var startYear = $("#startYear").val();
+        var endYear = $("#endYear").val();
+        var numberOfRecords = $("#numberOfRecords").val();
+        var yearRange = null;
+
+        var query = searchTerm + startYear + endYear;
+        
+        //checks to see if there are years entered in the form for the search
+        function createYearRange(startYear,endYear) {
+            if( startYear == "" ){
+                console.log("empty")
+            }
+        }
+        createYearRange(startYear,endYear)
+        console.log(startYear)
+
+        
+
+        var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=bd2ce8623fb048b697f2ddbc2abef055&q=" + query;
         $.ajax({
             url: queryURL,
             method: "GET"
@@ -12,6 +34,8 @@ $(document).ready(function (){
 
             var results = response.response.docs;
             console.log(results);
+
+            results
         })
     });
 
