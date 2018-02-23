3c8d1a4f9f140eeb87a9645cada7f9d7562345e5
Apollo <jackson.harris3@gmail.com>
2018-02-22 20:55:57 -0800

    add ajax call test for NYTimes API

--- assets/javascript/backend.js
+++ assets/javascript/backend.js
@@ -1,8 +1,9 @@
 $(document).ready(function (){
     console.log("test")
 
-    $("#searchButton").on("click", function () {
-
+    $("#searchButton").on("click", function (event) {
+        event.preventDefault()
+        console.log("button press")
         var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=bd2ce8623fb048b697f2ddbc2abef055&q=" + "banjo";
         $.ajax({
             url: queryURL,
