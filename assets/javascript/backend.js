b8ea29bc57abcb8abbc0d15b704bc302ab3cd04b
Apollo <jackson.harris3@gmail.com>
2018-02-22 20:47:34 -0800

    add ajax request

--- assets/javascript/backend.js
+++ assets/javascript/backend.js
@@ -1,3 +1,17 @@
 $(document).ready(function (){
     console.log("test")
+
+    $("#searchButton").on("click", function () {
+
+        var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=bd2ce8623fb048b697f2ddbc2abef055&q=" + "banjo";
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+
+            var results = response.response.docs;
+            console.log(results);
+        })
+    });
+
 }) 
