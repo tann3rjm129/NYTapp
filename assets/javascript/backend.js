e9b9cc8e27b2d32773fb84c10cfe0c408a5cf68f
Tanner McClain <tann3rjm129@gmail.com>
2018-03-01 15:27:13 -0800

    added clear button function

--- assets/javascript/backend.js
+++ assets/javascript/backend.js
@@ -8,22 +8,18 @@ $(document).ready(function (){
         
         //defines query from search value on form
         var searchTerm = $("#searchTerm").val().trim();
-        var startYear = $("#startYear").val().trim();
-        var endYear = $("#endYear").val().trim();
+        var start = $("#startYear").val().trim();
+        var end = $("#endYear").val().trim();
         var numberOfRecords = $("#numberOfRecords").val().trim();
         // var yearRange = null;
 
         var query = searchTerm;
         
-        //checks to see if there are years entered in the form for the search
-        // function createYearRange(startYear,endYear) {
-        //     if( startYear == "" ){
-        //         console.log("empty")
-        //     }
-        // }
-        // createYearRange(startYear,endYear)
-        // console.log(startYear)
-
+        // date format
+        // 2018-02-15T07:00:30+0000
+        
+        var startYear = start + "-01-01T01:00:01+0000";
+        var endYear = end + "-12-31T12:59:59+0000";
         
 
         var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=bd2ce8623fb048b697f2ddbc2abef055&q=" + query;
@@ -34,25 +30,42 @@ $(document).ready(function (){
 
             $("#topArticles").empty();
 
-
             for ( var i=0; i<numberOfRecords; i++) {
 
-                var articleBlock = $("<div>");
-                articleBlock.addClass("card");
-                articleBlock.addClass("fonts");
-                articleBlock.addClass("articleCard");
+                // var articleDate = response.response.doc.pub_date; scope issue, can't identify [i] of docs
 
-                articleBlock.append("<h5 class='card-body card-title lines' style='margin-top: 20px;'>"+ response.response.docs[i].headline.main +"</h5>" +
-               ("<p class='card-body card-text lines'>"+ response.response.docs[i].section_name +"</p>") +
-                ("<p class='card-body card-text lines'>"+ response.response.docs[i].pub_date +"</p>") +
-                ("<p class='card-body card-text lines'>"+ response.response.docs[i].snippet +"</p>") +
-                ("<p class='card-body card-text lines'>"+ response.response.docs[i].byline.original +"</p>") +
-                ("<a href='"+response.response.docs[i].web_url+"' class='btn btn-danger card-body lines' style='margin-bottom:20px; margin-left: 20px;''>Read more here</a>"));
+                // if ( articleDate > startYear && articleDate < endYear) {
 
-                $("#topArticles").append(articleBlock);
+
+                        var articleBlock = $("<div>");
+                        articleBlock.addClass("card");
+                        articleBlock.addClass("fonts");
+                        articleBlock.addClass("articleCard");
+
+                        articleBlock.append("<h5 class='card-body card-title lines' style='margin-top: 20px;'>"+ response.response.docs[i].headline.main +"</h5>" +
+                       ("<p class='card-body card-text lines'>"+ response.response.docs[i].section_name +"</p>") +
+                        ("<p class='card-body card-text lines'>"+ response.response.docs[i].pub_date +"</p>") +
+                        ("<p class='card-body card-text lines'>"+ response.response.docs[i].snippet +"</p>") +
+                        ("<p class='card-body card-text lines'>"+ response.response.docs[i].byline.original +"</p>") +
+                        ("<a href='"+response.response.docs[i].web_url+"' class='btn btn-danger card-body lines' style='margin-bottom:20px; margin-left: 20px;''>Read more here</a>"));
+
+                        $("#topArticles").append(articleBlock);
+                // };
             };
-        })
-        
+        });
+      
+      // clear button function
+        $("#clearButton").on("click", function (event) {
+
+        event.preventDefault();
+        $("#topArticles").empty();
+        start = $("#startYear").val("");
+        end = $("#endYear").val("");
+        searchTerm = $("#searchTerm").val("");
+
+
     });
+});
+
 
 });
