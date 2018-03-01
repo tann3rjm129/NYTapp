1ecb940cac9e0e91b8195e8cf2e2e1d15e16c1c0
Tanner McClain <tann3rjm129@gmail.com>
2018-03-01 14:26:50 -0800

    got the backend displaying, need to figure out year interval still

--- assets/javascript/backend.js
+++ assets/javascript/backend.js
@@ -1,28 +1,28 @@
 $(document).ready(function (){
-    console.log("test")
+
 
     $("#searchButton").on("click", function (event) {
         
         //prevents submits by button
-        event.preventDefault()
+        event.preventDefault();
         
         //defines query from search value on form
-        var searchTerm = $("#searchTerm").val();
-        var startYear = $("#startYear").val();
-        var endYear = $("#endYear").val();
-        var numberOfRecords = $("#numberOfRecords").val();
-        var yearRange = null;
+        var searchTerm = $("#searchTerm").val().trim();
+        var startYear = $("#startYear").val().trim();
+        var endYear = $("#endYear").val().trim();
+        var numberOfRecords = $("#numberOfRecords").val().trim();
+        // var yearRange = null;
 
-        var query = searchTerm + startYear + endYear;
+        var query = searchTerm;
         
         //checks to see if there are years entered in the form for the search
-        function createYearRange(startYear,endYear) {
-            if( startYear == "" ){
-                console.log("empty")
-            }
-        }
-        createYearRange(startYear,endYear)
-        console.log(startYear)
+        // function createYearRange(startYear,endYear) {
+        //     if( startYear == "" ){
+        //         console.log("empty")
+        //     }
+        // }
+        // createYearRange(startYear,endYear)
+        // console.log(startYear)
 
         
 
@@ -32,11 +32,26 @@ $(document).ready(function (){
             method: "GET"
         }).then(function (response) {
 
-            var results = response.response.docs;
-            console.log(results);
+            $("#topArticles").empty();
 
-            results
+            for ( var i=0; i<numberOfRecords; i++) {
+
+                var articleBlock = $("<div>");
+                articleBlock.addClass("card");
+                articleBlock.addClass("fonts");
+                articleBlock.addClass("articleCard");
+
+                articleBlock.append("<h5 class='card-body card-title lines' style='margin-top: 20px;'>"+ response.response.docs[i].headline.main +"</h5>" +
+               ("<p class='card-body card-text lines'>"+ response.response.docs[i].section_name +"</p>") +
+                ("<p class='card-body card-text lines'>"+ response.response.docs[i].pub_date +"</p>") +
+                ("<p class='card-body card-text lines'>"+ response.response.docs[i].snippet +"</p>") +
+                ("<p class='card-body card-text lines'>"+ response.response.docs[i].byline.original +"</p>") +
+                ("<a href='"+ response.response.docs[i].web_url +" class='btn btn-danger card-body lines' style='margin-bottom:20px; margin-left: 20px;''>Read more here</a>"));
+
+                $("#topArticles").append(articleBlock);
+            };
         })
+        
     });
 
-}) 
+});
