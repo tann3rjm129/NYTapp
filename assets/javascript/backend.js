8216a53049276b042b752aead0e6b5eeba7f7b77
Tanner McClain <tann3rjm129@gmail.com>
2018-03-01 18:22:32 -0800

    the search year interval seems to work, added start/end yr urls to query

--- assets/javascript/backend.js
+++ assets/javascript/backend.js
@@ -8,21 +8,33 @@ $(document).ready(function (){
         
         //defines query from search value on form
         var searchTerm = $("#searchTerm").val().trim();
-        var start = $("#startYear").val().trim();
-        var end = $("#endYear").val().trim();
+        var startYear = $("#startYear").val().trim();
+        var endYear = $("#endYear").val().trim();
         var numberOfRecords = $("#numberOfRecords").val().trim();
         // var yearRange = null;
 
         var query = searchTerm;
         
+        var startYearUrl;
+        var endYearUrl;
         // date format
         // 2018-02-15T07:00:30+0000
         
-        var startYear = start + "-01-01T01:00:01+0000";
-        var endYear = end + "-12-31T12:59:59+0000";
+        // var startYear = start + "0101";
+        // var endYear = end + "1231";
         
+        if (parseInt(startYear)) {
+            startYear += "0101";
+            startYearUrl = "&begin_date=" + startYear;
 
-        var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=bd2ce8623fb048b697f2ddbc2abef055&q=" + query;
+        }
+
+        else if (parseInt(endYear)){
+            endYear += "1231";
+            endYearUrl = "&end_date=" + endYear;
+        }
+
+        var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=bd2ce8623fb048b697f2ddbc2abef055&q=" + query + startYearUrl + endYearUrl;
         $.ajax({
             url: queryURL,
             method: "GET"
@@ -31,11 +43,6 @@ $(document).ready(function (){
             $("#topArticles").empty();
 
             for ( var i=0; i<numberOfRecords; i++) {
-
-                // var articleDate = response.response.doc.pub_date; scope issue, can't identify [i] of docs
-
-                // if ( articleDate > startYear && articleDate < endYear) {
-
 
                         var articleBlock = $("<div>");
                         articleBlock.addClass("card");
@@ -50,8 +57,7 @@ $(document).ready(function (){
                         ("<a href='"+response.response.docs[i].web_url+"' class='btn btn-danger card-body lines' style='margin-bottom:20px; margin-left: 20px;''>Read more here</a>"));
 
                         $("#topArticles").append(articleBlock);
-                // };
-            };
+                };
         });
       
       // clear button function
